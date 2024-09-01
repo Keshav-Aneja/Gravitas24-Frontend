090edeeffcf0351e32e2723bba35090c0e2e208f
rupaaksrinivas <rupaaksrinivas8@gmail.com>
2024-09-02 00:39:41 +0530

    feat: add auth page

---
+++ src/sections/auth/AuthBox.tsx
@@ -0,0 +1,15 @@
+import EventCard from "@/components/events/EventCard";
+import React from "react";
+
+const AuthBox = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <p className="text-3xl font-auxMono">
+        Getting Started...
+      </p>
+
+    </div>
+  );
+};
+
+export default AuthBox;
