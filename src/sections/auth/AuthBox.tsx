f63a15a9909db2240548888576712a2ee35a3f75
woaitsAryan <yoprince.ab@gmail.com>
2024-09-02 13:44:49 +0530

    fix: auth url fix

--- src/sections/auth/AuthBox.tsx
+++ src/sections/auth/AuthBox.tsx
@@ -9,7 +9,7 @@ const AuthBox = () => {
     <div className="w-full h-full flex flex-col items-center justify-center gap-8">
       <p className="text-3xl font-auxMono">Getting Started...</p>
       <a
-        href={`${BACKEND_URL}/auth/vit/google`}
+        href={`${BACKEND_URL}auth/vit/google`}
         className="bg-[#4285F4] py-2 w-[70%] flex items-center justify-center gap-6"
       >
         <Image
