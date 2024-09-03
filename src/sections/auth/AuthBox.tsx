106fca9bc1dd6da6f6d8d32a50f56dfb5d3cc3d4
woaitsAryan <yoprince.ab@gmail.com>
2024-09-03 21:42:46 +0530

    fix: auth flow

--- src/sections/auth/AuthBox.tsx
+++ src/sections/auth/AuthBox.tsx
@@ -3,12 +3,13 @@ import { BACKEND_URL } from "@/constants/routes";
 import { svgs } from "@/constants/svgs";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 const AuthBox = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-8">
       <p className="text-3xl font-auxMono">Getting Started...</p>
-      <a
-        href={`${BACKEND_URL}auth/vit/google`}
+      <Link
+        href={`${BACKEND_URL}/auth/vit/google`}
         className="bg-[#4285F4] py-2 w-[70%] flex items-center justify-center gap-6"
       >
         <Image
@@ -19,7 +20,7 @@ const AuthBox = () => {
           className="w-8 h-auto"
         />
         <p className="font-semibold text-white">Sign in with Google</p>
-      </a>
+      </Link>
     </div>
   );
 };
