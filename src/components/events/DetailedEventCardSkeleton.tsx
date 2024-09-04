3850deaf2a94fc832c051d5609c197dfb0827e7a
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-09-04 16:28:43 +0530

    feat: Detailed Event card skeleton loader

--- src/components/events/DetailedEventCardSkeleton.tsx
+++ src/components/events/DetailedEventCardSkeleton.tsx
@@ -20,140 +20,72 @@ import axiosInstance from "@/config/axios";
 import { CreateTransactionResponse } from "@/constants/types/transaction";
 import Scroller from "../common/Scroller";
 import { toast } from "@/hooks/use-toast";
+import images from "@/constants/images";
+import Skeleton from "../common/Skeleton";
 
-const DetailedEventCard = ({
-  id,
-  setEventName,
-  setLoading,
-}: {
-  id: string;
-  setEventName?: Dispatch<SetStateAction<string>>;
-  setLoading: Dispatch<SetStateAction<boolean>>;
-}) => {
-  const [eventDetails, setEventDetails] = useState<eventType | null>(null);
-  const startDate = eventDetails && new Date(eventDetails.startDate);
-  const endDate = eventDetails && new Date(eventDetails.endDate);
-  const [selectedSlot, setSelectedSlot] = useState("");
-  useEffect(() => {
-    (async () => {
-      try {
-        setLoading(true);
-        const eventDetails = await getEventDetails(id);
-        setLoading(false);
-        setEventDetails(eventDetails);
-        setEventName && setEventName(eventDetails.name);
-      } catch (error) {
-        setLoading(false);
-      }
-    })();
-  }, []);
-  if (!eventDetails) {
-    return null;
-  }
-
-  const registerEvent = async () => {
-    console.log("Registering event");
-    const token = Cookie.get("access_token");
-    console.log(token);
-    const payload = {
-      event_id: eventDetails.id,
-      event_slot_id: selectedSlot,
-    };
-    try {
-      const response = await axiosInstance.post("/registration/start", payload);
-      const data = response.data as CreateTransactionResponse;
-      const transactionFormData = data.data;
-      const form = document.createElement("form");
-      form.method = "POST";
-      form.action = "https://events.vit.ac.in/events/GRV24/cnfpay";
-      form.target = "_blank";
-      for (const [key, value] of Object.entries(transactionFormData)) {
-        const input = document.createElement("input");
-        input.type = "hidden";
-        input.name = key;
-        input.value = value as string;
-        form.appendChild(input);
-      }
-      document.body.appendChild(form);
-      form.submit();
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        toast({
-          title: "Error",
-          description: err.message,
-        });
-      }
-      console.log("An error occured. Kindly contact the admin.");
-    }
-  };
-
+const DetailedEventCardSkeleton = () => {
   return (
     <BorderBox className=" md:py-10 py-0 w-full  flex flex-col gap-8 px-0">
       <div className="w-full   flex flex-col md:flex-row items-start md:justify-between gap-4 md:gap-8 px-0">
         <div className="w-full md:w-[40%] relative ">
-          <div className="bg-white h-5 w-[60%] --clip-shape-card-image-top absolute top-0 left-0 flex items-center justify-center gap-2   ">
-            <span className="bg-primary h-[0.6rem] aspect-square rounded-full"></span>
-            <span className="bg-primary h-[0.6rem] aspect-square rounded-full"></span>
-            <span className="bg-primary h-[0.6rem] aspect-square rounded-full"></span>
-          </div>
-          <Image
-            className="w-full aspect-square border-[1px] border-black"
-            src={eventDetails.display}
-            alt=""
-            width={1000}
-            height={1000}
-          />
+          <Skeleton>
+            <Image
+              className="w-full aspect-square border-[1px] border-black"
+              src={images.gravitasLogo}
+              alt=""
+              width={1000}
+              height={1000}
+            />
+          </Skeleton>
         </div>
-        <section className="--main flex flex-col font-auxMono w-full md:w-[60%] px-2 md:px-0">
+        <section className="--main flex flex-col font-auxMono w-full md:w-[60%] px-2 md:px-0 gap-4">
           <h1 className="text-3xl md:text-4xl font-medium">
-            {eventDetails.name}
+            <Skeleton>FORK THIS EVENT</Skeleton>
           </h1>
-          <p className="text-primary text-sm md:text-lg">{eventDetails.club}</p>
+          <p className="text-primary text-sm md:text-lg">
+            <Skeleton>COMPUTER SOCIETEY OF INDIA</Skeleton>
+          </p>
           <div className="w-full h-[2px] bg-outline my-2 mb-4"></div>
           <section className=" flex items-center gap-0 min-w-fit text-[1rem] md:text-xl">
             <MdOutlineCurrencyRupee size={20} />
-            <p>
-              {eventDetails.price}
-              <span className="text-primary text-sm">(per person)</span>
-            </p>
+            <Skeleton>
+              <p>
+                10000
+                <span className="text-primary text-sm">(per person)</span>
+              </p>
+            </Skeleton>
           </section>
-          <section className="flex flex-col gap-2 text-sm md:text-[1rem]">
-            {eventDetails.description}
-          </section>
+          <Skeleton>
+            <section className="flex flex-col gap-2 text-sm md:text-[1rem]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit modi
+              suscipit earum sunt excepturi facere eveniet facilis doloremque
+              nulla nemo quo non molestias, porro iusto obcaecati.
+            </section>
+          </Skeleton>
           <div className="flex flex-col md:flex-row md:items-stretch gap-4 md:gap-16 mt-6">
             <ArrowBox className="w-full md:w-fit flex flex-col gap-1 md:block items-center justify-center text-black text-xs md:text-sm font-auxMono">
               <h1 className="text-secondary text-[1rem] md:text-lg font-semibold">
-                VENUE
+                <Skeleton>VENUE</Skeleton>
               </h1>
-              {eventDetails.slots?.map((slot, index) => (
-                <h2 key={index}>{slot?.venue}</h2>
-              ))}
+              <Skeleton>
+                {[1, 2, 3, 4].map((slot, index) => (
+                  <h2 key={index} className="mt-1">
+                    SLOT SLOT
+                  </h2>
+                ))}
+              </Skeleton>
             </ArrowBox>
             <ArrowBox className="w-full flex md:w-fit flex-col gap-1 md:block items-center justify-center text-black text-xs md:text-sm font-auxMono">
-              <h1 className="text-secondary text-[1rem] md:text-lg font-semibold">
-                NO OF PARTICIPANTS
-              </h1>
-              <h2>SOLO MEMBER</h2>
+              <Skeleton>
+                <h1 className="text-secondary text-[1rem] md:text-lg font-semibold">
+                  NO OF PARTICIPANTS
+                </h1>
+              </Skeleton>
+              <Skeleton>
+                <h2 className="mt-2">SOLO MEMBER</h2>
+              </Skeleton>
             </ArrowBox>
           </div>
-          <ArrowBox className="text-black text-sm font-auxMono w-full flex flex-col items-center gap-0 md:gap-3 mt-6">
-            <h1 className="text-secondary text-lg font-semibold">SLOTS</h1>
-            <div className="w-full gap-1">
-              <Scroller
-                uniqueName="slots"
-                className="auto-cols-[80%] md:auto-cols-[40%]  w-full mx-auto"
-              >
-                {eventDetails.slots?.map((slot, index) => (
-                  <SlotBox
-                    key={index}
-                    data={slot}
-                    setSlot={setSelectedSlot}
-                    selectedSlot={selectedSlot}
-                  />
-                ))}
-              </Scroller>
-            </div>
-          </ArrowBox>
         </section>
       </div>
       <div className="w-full  flex items-center justify-end mt-0 font-auxMono md:px-6">
@@ -197,18 +129,21 @@ const DetailedEventCard = ({
             </p>
           )}
         </span> */}
-        <button
-          className="text-white bg-primary h-full  p-4 w-full md:w-fit px-16"
-          onClick={registerEvent}
-        >
-          REGISTER
-        </button>
+        <Skeleton>
+          {" "}
+          <button
+            className="text-white bg-primary h-full  p-4 w-full md:w-fit px-16"
+            disabled
+          >
+            REGISTER
+          </button>
+        </Skeleton>
       </div>
     </BorderBox>
   );
 };
 
-export default DetailedEventCard;
+export default DetailedEventCardSkeleton;
 
 function ArrowBox({
   children,
