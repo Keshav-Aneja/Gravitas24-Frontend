1fc6d201001f836aa13370d94e5f16c1331de276
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-09-12 17:33:29 +0530

    fix: merch button fix

--- src/sections/landing/MiniHero.tsx
+++ src/sections/landing/MiniHero.tsx
@@ -71,7 +71,7 @@ function EventBtn() {
 }
 function MerchBtn() {
   return (
-    <Link href={FAQ_PAGE}>
+    <Link href={MERCH_PAGE}>
       <div className="relative w-32 md:w-48 scale-x-110 font-auxMono top-[12px] cursor-pointer hover:scale-x-[115%] hover:scale-y-[105%] transition-all duration-100 ease-linear active:scale-x-110 active:scale-y-[100%]">
         <Image
           src={svgs.HeroBtnB}
@@ -81,7 +81,7 @@ function MerchBtn() {
           className="w-full h-auto scale-y-[130%]"
         />
         <p className="absolute top-1 right-10 font-medium text-xs md:text-[1rem] tracking-widest text-nowrap ">
-          FAQs
+          MERCH
         </p>
       </div>
     </Link>
