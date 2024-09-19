5d9152eedb123395b48550dd502e03cbc39cf22a
Anuj Parihar <anujparihar@yahoo.com>
2024-09-19 12:57:50 +0530

    Homepage popup (#3)
    * feat: add homepage popup
    
    * feat: add home page popup video
    
    * styles: hover bg color
    
    * update video format to webm
    
    * popup link
    
    * External students popup content (#4)
    
    * popup content
    
    * popup content
    
    * popup content
    
    ---------
    
    Co-authored-by: RupaakSrinivas <rupaaksrinivas8@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,25 @@ import Gallery from "@/sections/landing/gallery";
 import Footer from "@/sections/landing/Footer";
 import AboutVIT from "@/sections/landing/AboutVIT";
 import { ReactLenis } from "@studio-freight/react-lenis";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import images from "@/constants/images";
 import MiniHero from "@/sections/landing/MiniHero";
 import { toast } from "@/hooks/use-toast";
 import Head from "next/head";
 import Cookies from "js-cookie";
+
+import {
+  Dialog,
+  DialogContent,
+} from "@/components/ui/dialog";
+
 export default function Home() {
-  // useEffect(() => {
-  //   toast({
-  //     title: "Test",
-  //     description: "lorem ipsum dolor amet",
-  //   });
-  // }, []);
+  const [showDialog, setShowDialog] = useState(false);
+
+  useEffect(() => {
+    setShowDialog(true);
+  }, []);
 
   return (
     <main className="bg-base w-full pb-6">
@@ -37,6 +42,36 @@ export default function Home() {
           orientation: "vertical",
         }}
       >
+        <Dialog
+          open={showDialog}
+          onOpenChange={() => {
+            setShowDialog(false);
+            return false;
+          }}
+        >
+          <DialogContent className="z-[500] w-full md:w-[60vw] fixed p-8 bg-primary border-primary">
+            <video
+              src="/video/laserShow.webm"
+              width={1920}
+              height={1080}
+              className="md:w-[60vw] z-[50]"
+              style={{ zIndex: 50 }}
+              playsInline={true}
+              autoPlay={true}
+              muted={true}
+              loop={true}
+              controls={true}
+              controlsList="nodownload nofullscreen noremoteplayback"
+            />
+            <a
+              href="https://gravitas.vit.ac.in/events/f2ad2286-6a52-4645-8b2f-635aa7fe635d"
+              className="px-4 py-2 bg-primary transition-all duration-100 ease-linear text-white font-auxMono relative border-2 text-center border-white hover:bg-primaryLight"
+            >
+              Register Now !!!
+            </a>
+          </DialogContent>
+        </Dialog>
+
         <MiniHero />
         <Hero />
         <Timer />
