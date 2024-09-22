d8090f7286526bffdbbe0c14d433afd6b137bbfa
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-09-22 23:00:20 +0530

    fix: patron label ui

--- src/components/landing/PatronCard.tsx
+++ src/components/landing/PatronCard.tsx
@@ -31,11 +31,6 @@ const PatronCard = ({ className, image, name, desig, label }: Props) => {
             <h1 className="h-[2lh]">{name}</h1>
             <h2 className="text-[1rem] text-primary">{desig}</h2>
           </div>
-          {label && (
-            <div className="absolute --patron-floater bg-primary right-0 top-[10%] p-1 px-2 text-center text-white font-auxMono font-normal ">
-              <h3 className="text-[0.65rem] lg:text-sm ">{label}</h3>
-            </div>
-          )}
         </>
       ) : (
         <div className="text-2xl font-auxMono font-medium">
